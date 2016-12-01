3d1e93640ca06ec68df9063a89a34debc767650d
ASUS\CEDRIC <ced8014@gmail.com>
2016-12-02 00:50:50 +0100

    [Ajout]view Connexion

---
+++ private/Views/view_Connexion.php
@@ -0,0 +1,14 @@
+<form method="post" action="../Controlers/ControllerConnexion.php">
+    Se connecter en tant qu'immigré
+    Pseudo :
+    <input type="text" name="login">
+    Mot de passe :
+    <input type="password" name="mdp">
+    <input type="submit" name="Valider_Immigrant">
+    Se connecter en tant que benevole
+    Pseudo :
+    <input type="text" name="login">
+    Mot de passe :
+    <input type="password" name="mdp">
+    <input type="submit" name="Valider_Immigrant">
+</form>
