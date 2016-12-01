e5cff29439e74737cb1eb400c06eef1c951d2a88
BIGOURD Paul <paul.bigourd@etud.u-picardie.fr>
2016-12-01 20:18:12 +0100

    Ajout de l'index
    page contenant les infos de l'équipe

---
+++ index_nuit.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<meta charset="UTF-8">
+<html>
+<head>
+	<meta name="viewport" content="width=device-width, initial-scale=1" >
+	<h1>Les poulpes Soyeux </h1>
+</head>
+
+<body>
+	<h2> Les membres de l'équipe: <h2>
+		<p> Leader: Flauss Tanguy </p>
+		<p> Hoch Lucas </p>
+		<p> Henry Pierre-Emmanuel</p>
+		<p> Hoch Lucas </p>
+		<p> Brassart Cédric </p>
+		<p> Carre Gabriel </p>
+		<p> Leleux Coralie </p>
+		<p>Bigourd Paul</p>
+		
+		
+</body>
+
+<footer>
+</footer>
+
+</html>
