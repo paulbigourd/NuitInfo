59567f45cd797cce6690c2c045f4886f18721a5e
lucas <lucash1712@hotmail.fr>
2016-12-02 07:06:12 +0100

    ajout art

--- private/Views/element/body.php
+++ private/Views/element/body.php
@@ -3,13 +3,45 @@
 	<body>
         <div class="row">
           <div class="col-md-3">
-          <p>bonjour</p>
+          <h1>Migrants en France: de la difficulté d’être en règle</h1>
+		  <img src="images/image1.jpg" alt="Photo de montagne" />
+		  <p>De la demande de visa à l’obtention d’un titre de séjour en passant par la demande du droit d’asile, les étrangers venant en France doivent faire face à de nombreuses difficultés. Alors que l’extrême droite estime que la France accueille trop d’étrangers, les démarches difficiles et les chiffres prouvent pourtant que le pays n’est pas laxiste en la matière. Un point sur les chiffres et les démarches.
+
+La délivrance de visas et de titres de séjours en France, qui permettent l’entrée et ensuite le séjour officiel sur le territoire, est souvent problématique. Il n’est pas rare d’entendre des témoignages de personnes ayant de grosses difficultés à obtenir leurs papiers, les refus de la préfecture n’étant pas toujours très clairs. La France, comme la plupart des pays dans le monde, délivre différents types de visas et titres de séjours : économique, familial, étudiants et stagiaires, touriste, transit… La procédure légale veut que la demande de visa soit effectuée auprès des autorités françaises avant la venue sur le territoire.
+
+Dans ce cadre, la France a délivré plus de visas en 2014 qu’en 2013, mais la hausse se limite à 13%. En 2014, ce sont 2,8 millions de visas qui ont été délivrés, mais la plupart, c'est-à-dire plus de 2,6 millions de visas sont accordés pour des courts séjours ou des visas de transit et de circulation, accordés surtout aux touristes. En réalité, seuls  182 549 visas longs séjours ont été distribués. Les visas longs séjours n’ont pour leur part augmenté que de 6% entre 2013 et 2014. Leur nombre avait même baissé en 2011 et 2012. Ces longs séjours concernent majoritairement les étudiants et les stagiaires (82 671), viennent ensuite les regroupements familiaux (48 631).
+
+Démarches des sans-papiers
+
+Mais une partie de la population étrangère sur le territoire français est présente de façon irrégulière. Ce sont les fameux sans-papiers. Pour eux, les démarches sont bien plus compliquées et ressemblent à un véritable casse-tête chinois. Toutes les démarches doivent se faire auprès de la préfecture de rattachement. Mais « il y a aujourd’hui 5 millions de passages par an en préfecture, pour 2,5 millions d’étrangers titulaires d’un titre de séjour », est-il précisé sur le site du gouvernement. Un manque de moyens administratifs qui crée des difficultés sur la simple question de l’accès aux bureaux des préfectures. Ce que confirme Lise Faron de la Cimade, une association de solidarité active avec les migrants, les réfugiés et les demandeurs d'asile : « Il y a des gens qui viennent la veille au soir, qui dorment devant, qui sont obligés de venir plusieurs soirs de suite, qui n’arrivent pas à avoir un ticket parce qu’il y a des numerus clausus pour accéder aux guichets ».
+
+
+« Une fois que vous accédez au guichet, il faut réussir à faire enregistrer votre dossier. Les préfectures demandent des listes de pièces, mais celles qui vont vraiment concerner la preuve de la situation que l’on invoque ne sont pas définies légalement. Chaque préfecture a un peu les mains libres et en réalité leurs exigences peuvent être tout à fait illégales ou abusives. Du point de vue des demandeurs, il est très difficile de savoir à quel moment un document qui vous est demandé est illégal », détaille Lise Faron.
+
+Des exigences à géométrie variable qui rendent difficile l’accompagnement des migrants : « Les exigences varient d’une préfecture à une autre, d’un agent préfectoral à un autre, regrette Lisa Faron. Vous pouvez accompagner un jour en tant que militant de la Cimade une personne étrangère pour une demande, cela se passe bien, et puis la semaine d’après vous accompagnez une personne qui vous semble être dans la même situation, ça se passe mal et vous ne savez pas pourquoi. On est dans un arbitraire très fort et dans un pouvoir très fort de la préfecture ». Des difficultés que confirme une avocate régulièrement confrontée aux problèmes des sans-papiers. Celle-ci porte un regard très critique sur les démarches administratives souvent contradictoires avec la réalité du quotidien des personnes.
+
+</p>
           </div>
           <div class="col-md-3">
-          hello
+                    <h1>Repères sur la santé des migrants </h1>
+		  <img src="images/image2.jpg" alt="Photo de montagne" />
+		  <p>Immigrés, étrangers, réfugiés, sans-papiers ou encore " personnes d'origine étrangère ", l'usage parfois indifférencié de ces termes témoigne d'une certaine confusion dans l'ensemble de la société sur le statut des migrants en France (voir encadré ci-contre), cette confusion étant imputable pour partie à la politisation des questions d'immigration et d'asile au cours des trente dernières années. Dans la même période, l'émergence de nouvelles priorités de santé publique, en premier lieu la lutte contre l'épidémie de VIH-sida, a progressivement conduit les pouvoirs publics à prendre en compte les spécificités des populations migrantes en matière de santé (1). Si les données d'épidémiologie médicale restent rares, la contribution des sciences humaines et l'expertise des acteurs de terrain ont permis d'améliorer les connaissances sur les facteurs de vulnérabilité de ces populations et d'adapter ainsi les actions à mettre en œuvre pour la santé des migrants.</p>
+
+
           </div>
           <div class="col-md-3">
-          Hola
+                              <h1>Tensions communautaires et difficultés pour les femmes d’être seules avec eux … </h1>
+		  <img src="images/image3.jpg" alt="Photo de montagne" />
+		  <p>20 migrants de Calais vont arriver dans les prochaines heures au CAO de Kerlaz (institut la Clarté) à proximité de Douarnenez. Au total entre 700 et 900 individus seront logés, nourris, blanchis dans des locaux en Bretagne (B5)
+
+Comme dans tous les centres d’accueil et d’orientation – et contrairement aux propos véhiculés par des élus locaux peu scrupuleux dans toute la Bretagne – il s’agit essentiellement d’hommes, célibataires, et en provenance d’Afrique noire.  Les fameux «réfugiés syriens» de Calais ne représentent qu’une petite minorité. Il s’agit donc principalement d’une immigration économique, contrairement à ce qu’ont affirmé avec force durant des mois journalistes subventionnés, élus et représentants de l’État.
+
+A Kerlaz, ce n’est pas la première fois que des migrants sont accueillis cette année. Une vingtaine y séjournait également ces derniers mois. Un séjour qui ne s’est pas passé sans encombre notamment concernant la place des femmes bénévoles avec les migrants hommes ou la cohabitation entre nationalités différentes.
+
+Breizh-info a pu ainsi prendre connaissance d’un  compte rendu – en date d’août 2016 – de la réunion du groupe de bénévoles de Kerlaz, qui s’occupent des migrants.  Voilà ce qu’on peut lire dans ce document – établi, rappelons-le  par des bénévoles favorables aux migrants – sur les problèmes rencontrés :
+
+</p>
+
           </div>  
         </div>
 	</body>
