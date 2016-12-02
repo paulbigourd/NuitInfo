5ba58f1ef60f4152065c249dc09b0b8f11591914
lucas <lucash1712@hotmail.fr>
2016-12-02 07:14:16 +0100

    correction erreur de noob

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <body>
         <?php include('private/Views/element/header.php'); ?>
         <main>
-            <?php include('private\Views\element\body.php');?>
+            <?php include('private/Views/element/body.php');?>
         </main>
     </body>
 </html>
