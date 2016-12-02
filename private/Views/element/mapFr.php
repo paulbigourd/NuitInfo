f765929c902a1f85e292e0f5a8b6f8c876a808be
lucas <lucash1712@hotmail.fr>
2016-12-02 07:29:40 +0100

    mdr

--- private/Views/element/mapFr.php
+++ private/Views/element/mapFr.php
@@ -4,17 +4,17 @@
     <!-- Placed at the end of the document so the pages load faster -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
     <script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery.min.js"><\/script>')</script>
-    <script src="../../dist/js/bootstrap.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"></script>
     <script src="../../assets/js/docs.min.js"></script>
     <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
     <script src="../../assets/js/ie10-viewport-bug-workaround.js"></script>
 
-    <link href="jqvmap.css" media="screen" rel="stylesheet" type="text/css" />
+    <link href="./private/Views/element/jqvmap.css" media="screen" rel="stylesheet" type="text/css" />
     
     <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js" type="text/javascript"></script>
-    <script src="jquery.vmap.js" type="text/javascript"></script>
-    <script src="jquery.vmap.france.js" type="text/javascript"></script>
-	<script src="jquery.vmap.colorsFrance.js" type="text/javascript"></script>
+    <script src="./private/Views/element/jquery.vmap.js" type="text/javascript"></script>
+    <script src="./private/Views/element/jquery.vmap.france.js" type="text/javascript"></script>
+	<script src="./private/Views/element/jquery.vmap.colorsFrance.js" type="text/javascript"></script>
     
 	<script type="text/javascript">
 	$(document).ready(function() {
