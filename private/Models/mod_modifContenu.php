de007fa22d4618a9e08e0c5649a2c49485651fcf
ASUS\CEDRIC <ced8014@gmail.com>
2016-12-02 05:35:40 +0100

    modif_Pass

--- private/Models/mod_modifContenu.php
+++ private/Models/mod_modifContenu.php
@@ -1,5 +1,5 @@
 <?php
-$db=new PDO("mysql:host=localhost;port=3306;dbname=nuit_info","root","");
+$db=new PDO("mysql:host=localhost;port=3306;dbname=nuit_info","root","iut");
 $db->modifContenu (PDO::ERRMODE_EXCEPTION);
 function modifContenu($titre,$nouv)
 {$query-> $bdd->prepare('UPDATE articles SET contenu=nouv where titre=titre.articles');
--- private/Models/mod_suppMignant.php
+++ private/Models/mod_suppMignant.php
@@ -1,5 +1,5 @@
 <?php
-$db=new PDO("mysql:host=localhost;port=3306;dbname=nuit_info","root","");
+$db=new PDO("mysql:host=localhost;port=3306;dbname=nuit_info","root","iut");
 $db->suppMigrant (PDO::ERRMODE_EXCEPTION);
 function suppMigrant($pseudo)
 {$query-> $bdd->prepare('DELETE pseudo, id, mdp, localisation, langue, age, sexe from benevoles where login=login.mignants');
