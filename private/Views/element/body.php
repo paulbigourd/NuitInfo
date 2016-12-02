ff46802b6d04271927341cb414974464b9031eae
BIGOURD Paul <paul.bigourd@etud.u-picardie.fr>
2016-12-02 04:36:50 +0100

    début intégration css

---
+++ private/Views/element/body.php
@@ -0,0 +1,17 @@
+    <html>
+     
+	<body>
+        <div class="row">
+          <div class="col-md-3">
+          <p>bonjour</p>
+          </div>
+          <div class="col-md-3">
+          hello
+          </div>
+          <div class="col-md-3">
+          Hola
+          </div>  
+        </div>
+	</body>
+
+</html>
