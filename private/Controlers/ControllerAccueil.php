3cd7f349b4d4c9a7ed522cd97cff32bd8d956fa8
lucas <lucash1712@hotmail.fr>
2016-12-02 04:51:16 +0100

    modif

--- private/Controlers/ControllerAccueil.php
+++ private/Controlers/ControllerAccueil.php
@@ -1,7 +1,7 @@
 <?php
-	include('private/Models/MOD_retArt');
+	include('private/Models/MOD_retArt.php');
 	
 	$tab = retArticles();
 	
-	require('private/Views/view_Accueil');
+	require('private/Views/view_Accueil.php');
 ?>
