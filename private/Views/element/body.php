0f9b0402e2a1c8dfa3a307c4ee7397f9d2ac4048
lucas <lucash1712@hotmail.fr>
2016-12-02 07:22:48 +0100

    hum hum

--- private/Views/element/body.php
+++ private/Views/element/body.php
@@ -4,7 +4,7 @@
         <div class="row">
           <div class="col-md-3">
           <h1>Migrants en France: de la difficulté d’être en règle</h1>
-		  <img src="./private/Views/element/images/img1.jpg" alt="Photo de montagne" />
+		  <img src="./private/Views/element/img1.jpg" alt="Photo de montagne" />
 		  <p>De la demande de visa à l’obtention d’un titre de séjour en passant par la demande du droit d’asile, les étrangers venant en France doivent faire face à de nombreuses difficultés. Alors que l’extrême droite estime que la France accueille trop d’étrangers, les démarches difficiles et les chiffres prouvent pourtant que le pays n’est pas laxiste en la matière. Un point sur les chiffres et les démarches.
 
 La délivrance de visas et de titres de séjours en France, qui permettent l’entrée et ensuite le séjour officiel sur le territoire, est souvent problématique. Il n’est pas rare d’entendre des témoignages de personnes ayant de grosses difficultés à obtenir leurs papiers, les refus de la préfecture n’étant pas toujours très clairs. La France, comme la plupart des pays dans le monde, délivre différents types de visas et titres de séjours : économique, familial, étudiants et stagiaires, touriste, transit… La procédure légale veut que la demande de visa soit effectuée auprès des autorités françaises avant la venue sur le territoire.
@@ -18,14 +18,14 @@ Démarches des sans-papiers
           </div>
           <div class="col-md-3">
                     <h1>Repères sur la santé des migrants </h1>
-		  <img src="./private/Views/element/images/img2.jpg" alt="Photo de montagne" />
+		  <img src="./private/Views/element/img2.jpg" alt="Photo de montagne" />
 		  <p>Immigrés, étrangers, réfugiés, sans-papiers ou encore " personnes d'origine étrangère ", l'usage parfois indifférencié de ces termes témoigne d'une certaine confusion dans l'ensemble de la société sur le statut des migrants en France (voir encadré ci-contre), cette confusion étant imputable pour partie à la politisation des questions d'immigration et d'asile au cours des trente dernières années. Dans la même période, l'émergence de nouvelles priorités de santé publique, en premier lieu la lutte contre l'épidémie de VIH-sida, a progressivement conduit les pouvoirs publics à prendre en compte les spécificités des populations migrantes en matière de santé (1). Si les données d'épidémiologie médicale restent rares, la contribution des sciences humaines et l'expertise des acteurs de terrain ont permis d'améliorer les connaissances sur les facteurs de vulnérabilité de ces populations et d'adapter ainsi les actions à mettre en œuvre pour la santé des migrants.</p>
 
 
           </div>
           <div class="col-md-3">
                               <h1>Tensions communautaires et difficultés pour les femmes d’être seules avec eux … </h1>
-		  <img src="./private/Views/element/images/img3.jpg" alt="Photo de montagne" />
+		  <img src="./private/Views/element/img3.jpg" alt="Photo de montagne" />
 		  <p>20 migrants de Calais vont arriver dans les prochaines heures au CAO de Kerlaz (institut la Clarté) à proximité de Douarnenez. Au total entre 700 et 900 individus seront logés, nourris, blanchis dans des locaux en Bretagne (B5)
 
 Comme dans tous les centres d’accueil et d’orientation – et contrairement aux propos véhiculés par des élus locaux peu scrupuleux dans toute la Bretagne – il s’agit essentiellement d’hommes, célibataires, et en provenance d’Afrique noire.  Les fameux «réfugiés syriens» de Calais ne représentent qu’une petite minorité. Il s’agit donc principalement d’une immigration économique, contrairement à ce qu’ont affirmé avec force durant des mois journalistes subventionnés, élus et représentants de l’État.
